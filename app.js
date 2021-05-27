28c0b143f0e425f89588506c0df597e37ae32928
Per Kristian <kristiansvanberg@gmail.com>
2021-05-27 23:56:55 +0200

    Added streams for better performance, data is being pulled in, in chunks

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res) {
 
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello World!\n');
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	fs.createReadStream(__dirname + '/index.htm').pipe(res);
 
 }).listen(1337, '127.0.0.1');
